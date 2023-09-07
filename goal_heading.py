0d1607e0ca59db46fe953992cd1ba53fc0d776fc
WHK-Scbert <63253621+WHK-Scbert@users.noreply.github.com>
2023-09-07 09:59:32 -0600

    goal_heading.py Working

--- goal_heading.py
+++ goal_heading.py
@@ -10,36 +10,24 @@ class GoalHeadingNode(Node):
     def __init__(self):
         super().__init__('goal_heading')
         self.data_publisher = self.create_publisher(Float64, 'goal_heading_topic', 10)
-        self.url = 'https://192.168.50.5:8081/DOA_value.html/'
+        self.url = 'http://192.168.50.5:8081/DOA_value.html'
         self.timer = self.create_timer(1, self.fetch_and_publish_data)
 
     def fetch_and_publish_data(self):
         try:
             # Fetch data from the URL
             response = httpx.get(self.url)
+            raw_data = response.text
+            data_value = raw_data.strip().split(',')
 
             # Check if the request was successful
             if response.status_code == 200:
                 # Split the data by comma and get the second value
-                '''
-                data_values = response.text.strip().split(',')
-                if len(data_values) >= 2:
-                    second_value = float(data_values[1])  # Convert to float
-
-                    # Publish the second value as a Float64 to the ROS 2 topic
-                    msg = Float64()
-                    msg.data = second_value
-                    self.data_publisher.publish(msg)
-                    self.get_logger().info("Published goal heading: %.2f", second_value)
-                else:
-                    self.get_logger().warn("Data format is not as expected")
-            else:
-                self.get_logger().error("Failed to fetch data from the URL. Status code: %d", response.status_code)
-
-        except Exception as e:
-            self.get_logger().error("An error occurred: %s", str(e))
-            '''
-                print(response.text)
+                heading = float(data_value[1])
+                print("Heading is " + str(heading))
+                msg = Float64()
+                msg.data = heading
+                self.data_publisher.publish(msg)
         except Exception as e:
             self.get_logger().error("An error occurred: %s", str(e))
 
